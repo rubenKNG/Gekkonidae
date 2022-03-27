29042baa25b2f3a765f4f700bd9b670f63ebcf27
Ruben König <ruben.kng@web.de>
2022-03-27 19:29:19 +0200

    Moved boot logic to Gekkonidae.java

--- src/main/java/kng/ruben/gekkonidae/Gekkonidae.java
+++ src/main/java/kng/ruben/gekkonidae/Gekkonidae.java
@@ -1,12 +1,31 @@
 package kng.ruben.gekkonidae;
 
-public enum Gekkonidae {
+import kng.ruben.gekkonidae.commands.logic.CommandManager;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 
-    instance();
+import javax.security.auth.login.LoginException;
+
+public class Gekkonidae {
 
     public static final String COMMAND_PREFIX = "!";
 
-    Gekkonidae() {
+    public static void main(String[] args) throws LoginException {
+        if (args.length == 0) {
+            System.out.println("No token provided");
+            System.exit(1);
+        }
+
+        var jdaBuilder = JDABuilder.createDefault(args[0]);
+        jdaBuilder.setStatus(OnlineStatus.ONLINE);
+
+        jdaBuilder.addEventListeners(new CommandManager());
+        jdaBuilder.enableCache(CacheFlag.VOICE_STATE);
+        jdaBuilder.setActivity(Activity.listening("to commands"));
+
+        jdaBuilder.build();
     }
 
 }
